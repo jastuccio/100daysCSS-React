1725a4f7986521febb7c0cd2c640a826f27b14b7
joe <joe@astucc.io>
2017-12-19 13:13:22 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hello() {
+  return (
+    <div>Hello 100!!!</div>
+  )
+}
+
+ReactDOM.render(
+  <Hello/>, document.querySelector('#root')
+);
